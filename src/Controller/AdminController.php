ce419a58ba0e42b229a44330a0ed90b80b8bb633
Charley Le Berre <c.leberre@hotmail.fr>
2022-06-09 19:00:57 +0200

    maj roles

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -9,9 +9,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/admin', name: 'admin_')]
 class AdminController extends AbstractController
 {
-    #[Route('/admin', name: 'app_admin')]
+    #[Route('/', name: 'index')]
     public function index(ArticleRepository $articleRepository, SocialNetworkRepository $socialNetworkRepository, ContactRepository $contactRepository): Response
     {
         return $this->render('admin/index.html.twig', [
