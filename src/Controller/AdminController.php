44c646f7c5468890f03cd296b3f6607ff225a4f4
Charley Le Berre <c.leberre@hotmail.fr>
2022-06-09 16:05:19 +0200

    entity contact

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\SocialNetworkRepository;
 use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,10 +11,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class AdminController extends AbstractController
 {
     #[Route('/admin', name: 'app_admin')]
-    public function index(ArticleRepository $articleRepository): Response
+    public function index(ArticleRepository $articleRepository, SocialNetworkRepository $socialNetworkRepository): Response
     {
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
+            'social_networks' => $socialNetworkRepository->findBy([],['id'=>'DESC'],3),
             'article' => $articleRepository->findBy([],['id'=>'DESC'],3)
         ]);
     }
